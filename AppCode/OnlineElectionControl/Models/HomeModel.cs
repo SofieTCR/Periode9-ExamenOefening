56a5c222efc9434fd80d4129ce8206deb4f99af5
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-09-21 12:14:03 +0200

    Initial VS Setup
    - Added the VS project
    - Added the Unit Test project
    - Added a sample unit test
    - Used BuilldWebCompiler for LESS compilation.
    - Workflow for compiling and testing on push or pull request

---
+++ AppCode/OnlineElectionControl/Models/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineElectionControl.Models
+{
+    public class HomeModel
+    {
+        /// <summary>
+        /// A method to return a string array of the provided string, split on the letter t
+        /// </summary>
+        /// <param name="splitString">The string to be split</param>
+        /// <returns></returns>
+        public string[] SplitOnT(string splitString)
+        {
+            return splitString.Split('t');
+        }
+    }
+}
---
+++ AppCode/OnlineElectionControlTests/ModelTests/HomeModelTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using FluentAssertions;
+using OnlineElectionControl.Models;
+
+namespace OnlineElectionControl.Tests.ModelTests
+{
+    public class HomeModelTests
+    {
+        [Theory]
+        [InlineData("Some string with four t's in it, for real", 5, new[] { "Some s", "ring wi", "h four ", "'s in i", ", for real" })]
+        [InlineData("Test the string, testing", 6, new[] { "Tes", " ", "he s", "ring, ", "es", "ing" })]
+        [InlineData("A totally random test string", 6, new[] { "A ", "o", "ally random ", "es", " s", "ring" })]
+        [InlineData("This is just a test", 4, new[] { "This is jus", " a ", "es", "" })]
+        [InlineData("No t in this sentence!", 4, new[] { "No ", " in ", "his sen", "ence!" })]
+        [InlineData("Tiny test with multiple t's", 6, new[] { "Tiny ", "es", " wi", "h mul", "iple ", "'s" })]
+        [InlineData("The quick brown fox", 1, new[] { "The quick brown fox" })]
+
+
+        public void HomeModel_SplitOnT_ReturnStringArray(string inputString, int outputCount, string[] resultingArray)
+        {
+            // Arrange
+            var homeModel = new HomeModel();
+
+            // Act
+            var result = homeModel.SplitOnT(inputString);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+            result.Should().HaveCount(outputCount);
+            result.Should().BeEquivalentTo(resultingArray);
+        }
+    }
+}
