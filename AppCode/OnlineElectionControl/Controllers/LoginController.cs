7806d969ce219ee6c0f716f14cbc719033510178
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-10-05 14:30:35 +0200

    Login and Register

--- AppCode/OnlineElectionControl/Controllers/LoginController.cs
+++ AppCode/OnlineElectionControl/Controllers/LoginController.cs
@@ -5,15 +5,85 @@ namespace OnlineElectionControl.Controllers
 {
     public class LoginController : OECBaseController
     {
+        [HttpGet]
         public IActionResult Index()
         {
             if (Current.UserIsLoggedIn) return RedirectToAction("Index", "Home");
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Register()
+        {
+            if (Current.UserIsLoggedIn) return RedirectToAction("Index", "Home");
+            return View("Register");
+        }
+
         [HttpPost]
         public IActionResult AttemptLogin(string pUsername, string pPassword)
         {
+            TempData["pUsername"] = pUsername;
+            var tmpQuery = "SELECT Id FROM `user` WHERE Username = @pUsername";
+            var tmpParameters = new Dictionary<string, object> { { "@pUsername", pUsername } };
+
+            var tmpResult = Database.ExecuteQuery(pQuery: tmpQuery, pParameters: tmpParameters);
+
+            if (tmpResult.Count != 1)
+            {
+                TempData["Vml"] = new List<string> { "Onbekende gebruikersnaam!" };
+                return RedirectToAction("Index");
+            }
+
+            var tmpUser = new User(pId: (int) tmpResult[0]["Id"]);
+
+            if (!tmpUser.VerifyPassword(pPassword))
+            {
+                TempData["Vml"] = new List<string> { "Onjuist Wachtwoord!" };
+                return RedirectToAction("Index");
+            }
+
+            HttpContext.Session.SetInt32("LoggedInUserId", (int) tmpUser.UserId!);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult AttemptRegister(string pUsername
+                                           , string pPassword
+                                           , string pFirstName
+                                           , string pLastName
+                                           , DateTime pBirthdate
+                                           , string pCity
+                                           , string pEmail)
+        {
+            TempData["pUsername"] = pUsername;
+            TempData["pFirstName"] = pFirstName;
+            TempData["pLastName"] = pLastName;
+            TempData["pBirthdate"] = pBirthdate;
+            TempData["pCity"] = pCity;
+            TempData["pEmail"] = pEmail;
+
+            var tmpUser = new User(pUsername: pUsername
+                                 , pFirstName: pFirstName
+                                 , pLastName: pLastName
+                                 , pBirthdate: pBirthdate
+                                 , pCity: pCity
+                                 , pEmail: pEmail);
+
+            if (!tmpUser.SetPassword(pPassword))
+            {
+                TempData["Vml"] = tmpUser.Vml;
+                return RedirectToAction("Register");
+            }
+
+            if (tmpUser.ValidateObject()) tmpUser.Save();
+            else
+            {
+                TempData["Vml"] = tmpUser.Vml;
+                return RedirectToAction("Register");
+            }
+
+            HttpContext.Session.SetInt32("LoggedInUserId", (int) tmpUser.UserId!);
 
             return RedirectToAction("Index", "Home");
         }
