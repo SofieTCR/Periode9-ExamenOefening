52ff5f8b583b1aedc2ce6b4cd0300bb1750b4269
Sofie <sofiebrink@yahoo.com>
2024-10-07 11:33:19 +0200

    Fix Scaling

--- AppCode/OnlineElectionControl/Classes/Current.cs
+++ AppCode/OnlineElectionControl/Classes/Current.cs
@@ -5,5 +5,19 @@
         public static int? LoggedInUserId;
 
         public static bool UserIsLoggedIn => LoggedInUserId != null;
+
+        private static User _loggedInUser;
+
+        public static User? LoggedInUser
+        {
+            get
+            {
+                if (LoggedInUserId == null) return null;
+
+                if (_loggedInUser == null || _loggedInUser.UserId != LoggedInUserId) _loggedInUser = new User(pId: (int) LoggedInUserId);
+
+                return _loggedInUser;
+            }
+        }
     }
 }
