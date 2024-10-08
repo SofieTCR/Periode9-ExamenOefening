88c3e350d5970a1b62528c427e599e1749016f13
Jack <9019233@student.zadkine.nl>
2024-10-08 09:56:45 +0200

    Finished us02 backend

--- AppCode/OnlineElectionControl/Models/HomeModel.cs
+++ AppCode/OnlineElectionControl/Models/HomeModel.cs
@@ -14,24 +14,14 @@ namespace OnlineElectionControl.Models
             return splitString.Split('t');
         }
 
-        public HomeModel()
-        {
-            //PastElections = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.Completed }, pSortOrder: SortOrder.ASC, pMaxNumber: 5);
-            
-            //var tmpElection = new Election(pName: $"Landelijke Verkiezingen {DateTime.Now.AddDays(32).ToString("HH:mm:ss [dd-MM-yyyy]")}", pDescription: null, pDate: DateTime.Today.AddDays(32));
-            //tmpElection.Save();
-            //var tmpElections = Election.GetList();
+        public List<Election> GetPastElections => Election.GetList(
+          pStatus: new List<ElectionStatus> { ElectionStatus.Completed}
+        , pSortOrder: SortOrder.DESC
+        , pMaxNumber: 5);
 
-            //var completedElections = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.Completed });
-            //var inProgressElections = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.InProgress });
-            //var scheduledElections = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.Scheduled });
-
-            //var scheduledandcurrent = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.InProgress, ElectionStatus.Scheduled });
-
-            //var scheduledandcurrentsortedasc = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.InProgress, ElectionStatus.Scheduled }, pSortOrder: SortOrder.ASC);
-            //var scheduledandcurrentsorteddesc = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.InProgress, ElectionStatus.Scheduled }, pSortOrder: SortOrder.DESC);
-
-            //var scheduledandcurrentsortedasc5 = Election.GetList(pStatus: new List<ElectionStatus> { ElectionStatus.InProgress, ElectionStatus.Scheduled }, pSortOrder: SortOrder.ASC, pMaxNumber: 5);
-        }
+        public List<Election> GetFutureElections => Election.GetList(
+          pStatus: new List<ElectionStatus> { ElectionStatus.Scheduled }
+        , pSortOrder: SortOrder.DESC
+        , pMaxNumber: 5);
     }
 }
