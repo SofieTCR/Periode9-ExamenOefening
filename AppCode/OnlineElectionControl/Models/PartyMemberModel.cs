66ebe398b000a77ecc2eb16fd74dddea1f0265b3
Stacey Peters <9018900@student.zadkine.nl>
2024-10-15 11:21:30 +0200

    US04

---
+++ AppCode/OnlineElectionControl/Models/PartyMemberModel.cs
@@ -0,0 +1,12 @@
+﻿using OnlineElectionControl.Classes;
+namespace OnlineElectionControl.Models
+{
+    public class PartyMemberModel
+    {
+        public List<User> Members;
+        public PartyMemberModel(int pId)
+        {
+            Members = User.GetList(pPartyId: pId, pIncludingNonMembers: true);
+        }
+    }
+}
