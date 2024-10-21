984a17af5365425f99d0a49454386d63891de4e2
Jack <9019233@student.zadkine.nl>
2024-10-21 17:10:13 +0200

    Basic Logics for voting gathering voting results

---
+++ AppCode/OnlineElectionControl/Controllers/ResultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineElectionControl.Classes;
+using OnlineElectionControl.Models;
+
+namespace OnlineElectionControl.Controllers
+{
+    public class ResultController : Controller
+    {
+        public IActionResult Index(int? pId)
+        {
+            if (pId == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            try
+            {
+                return View(new ResultModel(pElectionId: (int)pId!));
+            }
+            catch
+            {
+                TempData.Clear();
+                TempData["Vml"] = new string[] { $"Election with Id {pId} not found." };
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ AppCode/OnlineElectionControl/Models/ResultModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineElectionControl.Classes;
+using System.Runtime.ExceptionServices;
+
+namespace OnlineElectionControl.Models
+{
+    public class ResultModel
+    {
+        public List<Result> Results { get; set; }
+
+        public ResultModel(int pElectionId)
+        {
+            Results = Result.GetList(pElectionId);
+        }
+    }
+}
